713a21a7e7f34bf9552d2bab79b4ec051f3d734d
harshturakhia <76800875+harshturakhia@users.noreply.github.com>
2022-02-18 16:57:04 +0530

    Add files via upload

---
+++ tut53.js
@@ -0,0 +1,14 @@
+console.log("this is tutorial 53");
+
+let name1 = "Harsh";
+let name2 = "Devanshee";
+let name3 = "Tapan";
+let name4 = "Krishna";
+let name5 = "Parth";
+
+console.log(name1 + " is a good boiii...");
+console.log(name2 + " is a good girll...");
+console.log(name3 + " is a good boiii...");
+console.log(name4 + " is a good girll...");
+console.log(name5 + " is a good boiii...");
+
